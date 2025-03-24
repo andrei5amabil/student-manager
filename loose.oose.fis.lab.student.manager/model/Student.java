53fcfc390811779257c370e00071f660c7ce8f54
julia.capitanescu@student.upt.ro <m53CrxK#>
2025-03-24 19:33:48 +0200

    ok

---
+++ loose.oose.fis.lab.student.manager/model/Student.java
@@ -0,0 +1,13 @@
+package loose.oose.fis.lab.student.manager.model;
+
+public class Student {
+   private String firstName;
+   private String lastName;
+   private int age;
+   private double meanGrade;
+
+   public Student() {
+   }
+
+
+}
